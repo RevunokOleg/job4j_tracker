178ebadced0e6c44b4582e30fb8bbc2a33bb8a89
Red Devil <revunokoleg@gmail.com>
2024-02-12 23:52:57 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,26 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    Broken() {
+        String name;
+    }
+
+    public static final String NEWVALUE = "";
+
+    public String surname;
+
+    private final int sizeOfEmpty = 10;
+
+    void echo() {
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+}
