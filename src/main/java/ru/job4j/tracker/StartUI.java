2a2477cfe6e4cf964cd626d04bc0e5a2a58f19eb
Red Devil <revunokoleg@gmail.com>
2024-02-28 21:34:22 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,5 +9,7 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = itemDate.getCreated().format(formatter);
         System.out.println(currentDateTimeFormat);
+        Item newDate = new Item();
+        System.out.println(newDate);
     }
 }
