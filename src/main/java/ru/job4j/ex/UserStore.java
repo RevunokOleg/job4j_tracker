6285d3671ab6211c0af9c9e3db015c12c3c14055
Red Devil <revunokoleg@gmail.com>
2024-03-14 20:30:51 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #486002]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -6,19 +6,16 @@ public class UserStore {
         for (User user : users) {
             if (login.equals(user.getUsername())) {
                 return user;
-            } else {
-                throw new UserNotFoundException("User not found");
             }
         }
-        return null;
+        throw new UserNotFoundException("User not found");
     }
 
     public static boolean validate(User user) throws UserInvalidException {
         if (!user.isValid() || user.getUsername().length() < 3) {
             throw new UserInvalidException("Invalid user");
-        } else {
-            return true;
         }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -27,7 +24,6 @@ public class UserStore {
         };
         try {
             User user = findUser(users, "Petr Arsentev");
-            assert user != null;
             if (validate(user)) {
                 System.out.println("This user has an access");
             }
