15a5845857a66b040519f4a4cb6a56f1754e5820
Red Devil <revunokoleg@gmail.com>
2024-04-17 21:40:52 +0300

    2. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Petr, Ivan, Stepan");
+        for (Object name : names) {
+            System.out.println(name);
+        }
+    }
+}
