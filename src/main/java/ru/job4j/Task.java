2b5a8454a113f3e081b6c8266f90e843df0101c4
Red Devil <revunokoleg@gmail.com>
2024-04-18 21:21:21 +0300

    Task125

--- src/main/java/ru/job4j/Task.java
+++ src/main/java/ru/job4j/Task.java
@@ -2,23 +2,23 @@ package ru.job4j;
 
     public class Task {
         public static void loop(int num) {
-            StringBuilder result1 = new StringBuilder();
-            StringBuilder result2 = new StringBuilder();
+            StringBuilder evenDigits = new StringBuilder();
+            StringBuilder oddDigits = new StringBuilder();
             int number;
             while (num != 0) {
                 number = num % 10;
                 if (number % 2 == 0) {
-                    result1.append(number).append(" ");
+                    evenDigits.append(number).append(" ");
                 } else {
-                    result2.append(number).append(" ");
+                    oddDigits.append(number).append(" ");
                 }
                 num /= 10;
             }
-            if (!(result1.isEmpty())) {
-                System.out.println(result1.reverse().toString().trim());
+            if (!evenDigits.isEmpty()) {
+                System.out.println(evenDigits.reverse().toString().trim());
             }
-            if (!(result2.isEmpty())) {
-                System.out.println(result2.reverse().toString().trim());
+            if (!oddDigits.isEmpty()) {
+                System.out.println(oddDigits.reverse().toString().trim());
             }
         }
 
