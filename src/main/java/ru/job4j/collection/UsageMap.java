35c234565f713bdac82a42ed993a221088eb2ca5
Red Devil <revunokoleg@gmail.com>
2024-04-27 09:30:35 +0300

    0. Map, HashMap [#212652 #493264]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,7 +5,13 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
+        map.put("revunokoleg@gmail.com", "Vasiliy Petrov");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("revunokoleg@gmail.com", "Revunok Oleg Alekseevich");
+        map.put("ivanov@gmail.com", "Ivan Ivanov");
+        map.put("stepanov@gmail.com", "Stepan Stepanov");
         map.put("revunokoleg@gmail.com", "Revunok Oleg");
+        map.put("parsentev@yandex.ru", "Petr Arsentev Sergeevich");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
