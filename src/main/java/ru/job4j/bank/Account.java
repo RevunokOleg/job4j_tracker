5e1782e424ffe59ad5c83d6796a9877a601f1c12
Red Devil <revunokoleg@gmail.com>
2024-05-22 20:58:02 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,31 +2,72 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * В данном классе реализована модель банковского счета.
+ * Эта модель содержит поля: баланс и реквизиты.
+ * @author Revunok Oleg
+ * @version 1.0
+ */
 public class Account {
+
+    /**
+     * Переменная хранит реквизиты счета.
+     */
     private String requisite;
+
+    /**
+     * Переменная хранит баланс счета.
+     */
     private double balance;
 
+    /**
+     * Конструктор объекта Account.
+     * @param requisite реквизиты счета.
+     * @param balance баланс счета.
+     */
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * Метод получения реквизитов счета.
+     * @return возвращет реквизиты счета.
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * Метод устанавливает реквизиты счета.
+     * @param requisite реквизиты счета.
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Метод получения баланса счета.
+     * @return возвращает баланс счета.
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Метод устанавливает баланс счета .
+     * @param balance баланс счета.
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Метод сравнения двух объектов.
+     * @param o объект сравнения.
+     * @return возвращет true, если объекты равны,
+     * либо false, если не равны.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +80,10 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * Метод возвращает хэш-код объекта на основе его реквизитов.
+     * @return возвращает хэш-код объекта.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
