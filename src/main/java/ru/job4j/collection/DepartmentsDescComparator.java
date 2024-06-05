590b72d4535de8ff79b8acf7f1f5c902f884ce24
Red Devil <revunokoleg@gmail.com>
2024-06-05 18:37:25 +0300

    3. Отсортировать департаменты [#34352 #499373]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -1,23 +1,15 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 public class DepartmentsDescComparator implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        List<String> splitLeft = Arrays.asList(left.split("/"));
-        List<String> splitRight = Arrays.asList(right.split("/"));
-        if (splitRight.get(0).compareTo(splitLeft.get(0)) == 0) {
-            int strLength = Math.min(splitLeft.size(), splitRight.size());
-            for (int i = 1; i < strLength; i++) {
-                int result = splitLeft.get(i).compareTo(splitRight.get(i));
-                if (result != 0) {
-                    return result;
-                }
-            }
-            return Integer.compare(splitLeft.size(), splitRight.size());
+        String[] splitLeft = left.split("/");
+        String[] splitRight = right.split("/");
+        int result = splitRight[0].compareTo(splitLeft[0]);
+        if (result == 0) {
+            return left.compareTo(right);
         }
         return right.compareTo(left);
     }
