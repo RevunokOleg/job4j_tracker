e8569ab53e3667dc95f14ffdb69eaa18aeaf9b55
Red Devil <revunokoleg@gmail.com>
2024-06-06 10:41:15 +0300

    3. Отсортировать департаменты [#34352 #499373]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -8,9 +8,6 @@ public class DepartmentsDescComparator implements Comparator<String> {
         String[] splitLeft = left.split("/");
         String[] splitRight = right.split("/");
         int result = splitRight[0].compareTo(splitLeft[0]);
-        if (result == 0) {
-            return left.compareTo(right);
-        }
-        return right.compareTo(left);
+        return result != 0 ? result : left.compareTo(right);
     }
 }
